702928fc5a3f76178a911ebedc31dc0ce0661bdf
unknown <taras.zdyrko.tr.2018@lpnu.ua>
2023-01-26 22:26:36 +0200

    ServiceClass

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tel_Book.Services;
+
+namespace Tel_Book
+{
+    internal class Program
+    {
+    static void Main(string[] args)
+        {
+            ContactsService ContactsService = new ContactsService();
+            Console.OutputEncoding = Encoding.GetEncoding(1251);
+
+            Console.WriteLine("Дай Боже!!\nЦе вроді як телефонна книга\n");
+
+            ContactsService.Contacts["nePidar"] = new List<string>() { "+380 68 300 4550", "123" };
+
+            ContactsService Cs = new ContactsService();
+
+            while (true) 
+            {
+                Console.WriteLine("\n\t1 - переглянути\n" +
+                "\t2 - додати\n" +
+                "\t3 - видалити\n" +
+                "\t4 - редагувати\n" +
+                "\t0 - Вихід");
+                Console.Write("\nОберіть опцію: ");
+                char option = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                if (option == '1')
+                    Cs.PrintContacts();
+                else if (option == '2')
+                    Cs.AddContact();
+                else if (option == '3')
+                    Cs.RemoveContact();
+                else if (option == '4')
+                    Cs.ContactUpdate();
+                else if (option == '0')
+                    break;
+                else
+                    Console.WriteLine("Ви ввлели зуєту");
+            }
+            Console.WriteLine();
+        } 
+    }
+}
---
+++ Services/ContactService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tel_Book.Services
+{
+    public class ContactsService
+    {
+        public Dictionary<string, List<string>> Contacts = new Dictionary<string, List<string>>();
+
+        public string EnterDataName() // Перенести в "Програмс"
+        {
+            string ContactName = Console.ReadLine();
+            return ContactName;
+        }
+
+        public List<string> EnterDataNumbers() // Перенести в "Програмс"
+        {
+            Console.Write("Кількість номерів: ");
+            uint nNumbers = uint.Parse(Console.ReadLine());
+
+            var ContactNumbers = new List<string>();
+            for (int i = 0; i < nNumbers; i++)
+            {
+                Console.Write("номер {0}: ", (i + 1));
+                ContactNumbers.Add(Console.ReadLine());
+            }
+            return ContactNumbers;
+        }
+        public void PrintContacts()
+        {
+            Console.WriteLine();
+            foreach (var contact in FetchContacts())
+            {
+                Console.WriteLine(contact.Key + " " + string.Join(", ", contact.Value.ToArray()));
+            }
+        }
+
+        public void AddContact()
+        {
+            Console.Write("\nВведіть Ім'я: ");
+            string ContactName = EnterDataName();
+            List<string> ContactNumbers = EnterDataNumbers();
+            Contacts[ContactName] = ContactNumbers;
+        }
+        public void RemoveContact()
+        {
+            Console.WriteLine("\nВведіть ім'я дегана");
+            string ContactName = EnterDataName();
+            Contacts.Remove(ContactName);
+        }
+
+        public void ContactUpdate()
+        {
+            RemoveContact();
+            AddContact();
+        }
+        Dictionary<string, List<string>> FetchContacts()
+        {
+            return Contacts;
+        }
+    }
+}
